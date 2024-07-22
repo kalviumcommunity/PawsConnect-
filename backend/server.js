054729ce3c8f411148f2185bff15a1b868c6e51c
yash <144336541+YashBodhe2004@users.noreply.github.com>
2024-07-22 11:57:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/ping', (req, res) => {
+app.get('/', (req, res) => {
   res.send('pong');
 });
 
