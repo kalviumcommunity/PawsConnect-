e68cb28d3b342cbebbc699ee7fb2b05a03805b94
yash <144336541+YashBodhe2004@users.noreply.github.com>
2024-10-08 10:40:51 +0530

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors'); 
-const Entity = require('./schema'); 
+const Entity = require('./schema');
 
 router.use(express.json());
 router.use(cors()); 
@@ -9,7 +9,7 @@ router.use(cors());
 
 router.get('/data', async (req, res) => {
     try {
-        const data = await Entity.find().maxTimeMS(20000).exec(); 
+        const data = await Entity.find().maxTimeMS(20000).exec();
         res.json(data);
     } catch (err) {
         console.error('Error in GET data request:', err);
@@ -25,6 +25,36 @@ router.post('/add-data', async (req, res) => {
     } catch (err) {
         console.error('Error adding new data:', err);
         res.status(500).json({ error: err.message || 'Internal Server Error' });
+    }
+});
+
+
+router.put('/update-data/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = await Entity.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+        if (!updatedData) {
+            return res.status(404).json({ error: 'Data not found' });
+        }
+        res.json(updatedData);
+    } catch (err) {
+        console.error('Error updating data:', err);
+        res.status(500).json({ error: err.message || 'Internal Server Error' });
+    }
+});
+
+
+router.delete('/delete-data/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedData = await Entity.findByIdAndDelete(id);
+        if (!deletedData) {
+            return res.status(404).json({ error: 'Data not found' });
+        }
+        res.json({ message: 'Data successfully deleted', deletedData });
+    } catch (err) {
+        console.error('Error deleting data:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
