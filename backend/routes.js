d02c8668a578f2856dbedf7945df7cef8f8a0a5e
yash <144336541+YashBodhe2004@users.noreply.github.com>
2024-10-08 10:36:35 +0530

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors'); 
-const Entity = require('./schema'); // Import Entity, not EntitySchema
+const Entity = require('./schema'); 
 
 router.use(express.json());
 router.use(cors()); 
 
-// GET all data
+
 router.get('/data', async (req, res) => {
     try {
-        const data = await Entity.find().maxTimeMS(20000).exec();
+        const data = await Entity.find().maxTimeMS(20000).exec(); 
         res.json(data);
     } catch (err) {
         console.error('Error in GET data request:', err);
@@ -17,11 +17,10 @@ router.get('/data', async (req, res) => {
     }
 });
 
-// Add new data
 router.post('/add-data', async (req, res) => {
     try {
         console.log("Received request body:", req.body); 
-        const newData = await Entity.create(req.body);
+        const newData = await Entity.create(req.body); 
         res.status(201).json(newData);
     } catch (err) {
         console.error('Error adding new data:', err);
